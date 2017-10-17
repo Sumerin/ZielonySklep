1995a9cf6b91f258d7038bee6d2524997d4c2c3f
sumerin <sumekpl@gmail.com>
2017-10-17 18:07:07 +0000

    First Start

---
+++ html/config/settings.inc.php
@@ -0,0 +1,16 @@
+<?php
+define('_DB_SERVER_', 'some-mysql');
+define('_DB_NAME_', 'prestashop');
+define('_DB_USER_', 'root');
+define('_DB_PASSWD_', 'admin');
+define('_DB_PREFIX_', 'ps_');
+define('_MYSQL_ENGINE_', 'InnoDB');
+define('_PS_CACHING_SYSTEM_', 'CacheMemcache');
+define('_PS_CACHE_ENABLED_', '0');
+define('_COOKIE_KEY_', 'GTgfcDHEjw5DpcsCkCRmG7jJHkodGIo84Cd6R6EAWLNbogFRyFzdDIJk');
+define('_COOKIE_IV_', 'BY7mF2aZ');
+define('_PS_CREATION_DATE_', '2017-10-17');
+if (!defined('_PS_VERSION_'))
+	define('_PS_VERSION_', '1.6.1.17');
+define('_RIJNDAEL_KEY_', 'kpT8pS29z9pzMmwDijhKmEbHg6gFb82O');
+define('_RIJNDAEL_IV_', 'P/STSDjlFESMBIGw1EFQCQ==');
